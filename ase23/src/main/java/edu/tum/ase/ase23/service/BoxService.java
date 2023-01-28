352ef52cb20bcbeecdb38161171f2551e1290148
muruvvetb <mbozkurtt76@gmail.com>
2023-01-29 00:02:35 +0100

    finish box

--- ase23/src/main/java/edu/tum/ase/ase23/service/BoxService.java
+++ ase23/src/main/java/edu/tum/ase/ase23/service/BoxService.java
@@ -6,31 +6,51 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class BoxService {
     @Autowired
-    BoxRepository boxRepo;
+    BoxRepository boxRepository;
 
     public List<Box> getAllBoxes() {
-        return boxRepo.findAll();
+        return boxRepository.findAll();
     }
 
     public Box createBox(Box box) {
-        return boxRepo.save(box);
+        return boxRepository.save(box);
     }
 
-    public Box findByName(String name) throws Exception {
-        return boxRepo.findByName(name).orElseThrow(() -> new Exception("No Box with name " + name));
+    public Box findById(String Id) throws Exception {
+        return boxRepository.findById(Id).orElseThrow(() -> new Exception("No Box with this ID " + Id));
     }
 
-    public Box findByStreetAddress(String streetAddress) throws Exception {
-        return boxRepo.findByStreetAddress(streetAddress).orElseThrow(() -> new Exception("No Box with this Street Address " + streetAddress));
+    public Box findByName(String Name) throws Exception {
+        return boxRepository.findByName(Name).orElseThrow(() -> new Exception("No Box with name " + Name));
     }
 
-    public Box findByAlive(Boolean alive) throws Exception {
-        return boxRepo.findByAlive(alive).orElseThrow(() -> new Exception("There is no any alive box!"));
+    public Box findByStreetAddress(String StreetAddress) throws Exception {
+        return boxRepository.findByStreetAddress(StreetAddress).orElseThrow(() -> new Exception("No Box with this Street Address " + StreetAddress));
     }
 
-    
+    public List<Box> findAllAlive(Boolean Alive) throws Exception {
+        List<Box> boxes = this.getAllBoxes();
+        return boxes.stream().filter(box -> box.getAlive().equals(Alive)).collect(Collectors.toList());
+    }
+
+
+
+    public Box update(String Id, Box box) throws Exception {
+        Box existingBox = boxRepository.findById(Id).orElseThrow(() -> new Exception("Box not found with id: " + Id));
+        existingBox.setName(box.getName());
+        existingBox.setStreetAddress(box.getStreetAddress());
+        existingBox.setAlive(box.getAlive());
+        return boxRepository.save(existingBox);
+    }
+
+    public Box delete(String Id) throws Exception {
+        Box deletedBox = boxRepository.findById(Id).orElseThrow(() -> new Exception("Box not found with id: " + Id));
+        deletedBox.setAlive(false);
+        return boxRepository.save(deletedBox);
+    }
 }
