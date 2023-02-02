f8d82d30210ebc14dd81e9b9cdb6d82478c919d5
Ihsan Soydemir <soydemir.ihsan@gmail.com>
2023-02-02 17:09:38 +0100

    Cleanup user service

--- ase23/src/main/java/edu/tum/ase/ase23/service/UserService.java
+++ ase23/src/main/java/edu/tum/ase/ase23/service/UserService.java
@@ -1,5 +1,6 @@
 package edu.tum.ase.ase23.service;
 
+import edu.tum.ase.ase23.model.RoleEnum;
 import edu.tum.ase.ase23.model.User;
 import edu.tum.ase.ase23.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,38 +15,19 @@ public class UserService {
     UserRepository userRepository;
 
     public List<User> getAllDeliverers() {
-        return userRepository.findByUserType("deliverer");
+        return userRepository.findByRoles(RoleEnum.ROLE_DELIVERER);
     }
 
     public List<User> getAllCustomers() {
-        return userRepository.findByUserType("customer");
+        return userRepository.findByRoles(RoleEnum.ROLE_CUSTOMER);
     }
 
     public List<User> getAllDispatchers() {
-        return userRepository.findByUserType("dispatcher");
+        return userRepository.findByRoles(RoleEnum.ROLE_DISPATCHER);
     }
 
     public User getUserById(String id) throws Exception {
         return userRepository.findById(id).orElseThrow(() -> new Exception("User cannot find by id " + id));
-    }
-
-    public void changePassword(String id, String oldPassword, String newPassword, Boolean isAdmin) throws Exception {
-        if (isAdmin == null) {
-            isAdmin = false;
-        }
-        User user = userRepository.findById(id).orElseThrow(() -> new Exception("User cannot find by id " + id));
-        if (!isAdmin) {
-            if (user.getPassword().equals(oldPassword)) {
-                user.setPassword(newPassword);
-            }
-            else{
-                throw new Exception("Passwords do not match!");
-            }
-        }
-        else {
-            user.setPassword(newPassword);
-        }
-        userRepository.save(user);
     }
 
 }
